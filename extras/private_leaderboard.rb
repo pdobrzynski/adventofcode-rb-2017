3ea9a02adbd1537b7969cca0286f3cab72e6052c
Peter Tseng <pht24@cornell.edu>
2017-12-06 23:53:36 -0800

    add private leaderboard parser

---
+++ extras/private_leaderboard.rb
@@ -0,0 +1,22 @@
+require 'json'
+require 'time'
+
+people = JSON.parse(ARGF.read)['members'].values
+
+max_day = people.flat_map { |v| v['completion_day_level'].keys }.map(&:to_i).max
+longest_name = people.map { |v| v['name'] || v['id'] }.map(&:size).max
+
+(1..max_day).each { |day|
+  (1..2).each { |part|
+    completing_people = people.map { |p|
+      ts = p.dig('completion_day_level', day.to_s, part.to_s, 'get_star_ts')
+      ts && [p['name'] || p['id'], Time.parse(ts)]
+    }.compact.to_h
+    puts "Day #{day} Part #{part}:"
+    fmt = "%#{longest_name}s %s %d"
+    completing_people.sort_by(&:last).each_with_index { |(p, t), i|
+      puts fmt % [p, t, people.size - i]
+    }
+    puts
+  }
+}
