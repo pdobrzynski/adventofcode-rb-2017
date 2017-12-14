ebd8141f592e848a259a309c0cd6a8d426660982
Peter Tseng <pht24@cornell.edu>
2017-12-14 01:57:25 -0800

    lib: Add Union Find

---
+++ lib/union_find.rb
@@ -0,0 +1,27 @@
+class UnionFind
+  def initialize(things)
+    @parent = things.map { |x| [x, x] }.to_h
+    @rank = things.map { |x| [x, 0] }.to_h
+  end
+
+  def union(x, y)
+    xp = find(x)
+    yp = find(y)
+
+    return if xp == yp
+
+    if @rank[xp] < @rank[yp]
+      @parent[xp] = yp
+    elsif @rank[xp] > @rank[yp]
+      @parent[yp] = xp
+    else
+      @parent[yp] = xp
+      @rank[xp] += 1
+    end
+  end
+
+  def find(x)
+    @parent[x] = find(@parent[x]) if @parent[x] != x
+    @parent[x]
+  end
+end
