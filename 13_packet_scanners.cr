b6d32a374cb49088818cd6c7bf51c7f4fd99a9cf
Peter Tseng <pht24@cornell.edu>
2017-12-14 04:18:50 -0800

    Day 13: Work in Crystal
    * include -> includes
    * use tuple in to_h
    * add a not_nil for a delete from hash
    * no trailing until allowed
    * no __END__

--- 13_packet_scanners.cr
+++ 13_packet_scanners.cr
@@ -1,8 +1,9 @@
 depths = (ARGV.empty? ? DATA : ARGF).each_line.map { |x|
-  x.scan(/\d+/).map(&:to_i)
-}.to_h.freeze
+  parts = x.split(':').map(&.to_i)
+  {parts[0], parts[1]}
+}.to_h
 
-periods = depths.map { |k, v| [k, 2 * (v - 1)] }.to_h.freeze
+periods = depths.map { |k, v| {k, 2 * (v - 1)} }.to_h
 
 puts periods.select { |k, v| k % v == 0 }.keys.map { |k| k * depths[k] }.sum
 
@@ -11,8 +12,8 @@ puts periods.select { |k, v| k % v == 0 }.keys.map { |k| k * depths[k] }.sum
 # The starting time is NOT congruent to -depth modulo period.
 # We combine all these forbidden times by period.
 def forbidden_starts(periods)
-  periods.group_by(&:last).map { |period, depths|
-    [period, depths.map { |depth, _| -depth % period }.sort]
+  periods.group_by(&.last).map { |period, depths|
+    {period, depths.map { |depth, _| -depth % period }.sort}
   }.to_h
 end
 
@@ -25,9 +26,9 @@ def absorb_periods(forbidden_starts)
   periods.each_with_index { |p1, i|
     expand_into = periods[(i + 1)..-1].select { |x| x % p1 == 0 }
     next if expand_into.empty?
-    forbidden = forbidden_starts.delete(p1)
+    forbidden = forbidden_starts.delete(p1).not_nil!
     expand_into.each { |p2|
-      forbidden_starts[p2] |= (0...p2).select { |p| forbidden.include?(p % p1) }
+      forbidden_starts[p2] |= (0...p2).select { |p| forbidden.includes?(p % p1) }
     }
   }
 
@@ -42,19 +43,20 @@ single_openings, other_periods = forbidden_starts.partition { |p, ds|
 
 # For any periods with only one opening, we can combine them into one big period,
 # and this big period also only has one opening!
-base, period = single_openings.sort_by(&:first).reverse.reduce([0, 1]) { |(b, p1), (p2, ds)|
-  allowed_d = (0...p2).find { |d| !ds.include?(d) }
-  b += p1 until b % p2 == allowed_d
+base, period = single_openings.sort_by(&.first).reverse.reduce([0, 1]) { |(b, p1), (p2, ds)|
+  allowed_d = (0...p2).find { |d| !ds.includes?(d) }
+  until b % p2 == allowed_d
+    b += p1
+  end
   [b, p1.lcm(p2)]
 }
 
 puts base.step(by: period).find { |delay|
   # Now we only need to check all other periods.
-  other_periods.all? { |p, ds| !ds.include?(delay % p) }
+  other_periods.all? { |p, ds| !ds.includes?(delay % p) }
 }
 
-__END__
-0: 3
+DATA = "0: 3
 1: 2
 2: 6
 4: 4
@@ -96,4 +98,4 @@ __END__
 82: 14
 88: 18
 92: 14
-98: 18
+98: 18"
