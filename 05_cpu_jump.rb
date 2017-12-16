beafd90290270d9e11f349df63758e68a5a0e54c
Peter Tseng <pht24@cornell.edu>
2017-12-16 04:34:04 -0800

    Day 05: Use C
    This shows how to pass arrays.

--- 05_cpu_jump.rb
+++ 05_cpu_jump.rb
@@ -12,6 +12,18 @@ end
 
 input = (ARGV.empty? ? DATA : ARGF).each_line.map(&:to_i)
 
+c_lib = File.join(__dir__, 'c', 'lib05.so')
+if File.exist?(c_lib)
+  require 'fiddle'
+
+  lib = Fiddle.dlopen(c_lib)
+  ['part1', 'part2'].map { |f| Fiddle::Function.new(
+    lib[f], [Fiddle::TYPE_VOIDP, Fiddle::TYPE_SIZE_T], Fiddle::TYPE_SIZE_T,
+  )}.each { |f| puts f.call(Fiddle::Pointer[input.pack('i*')], input.size) }
+
+  exit 0
+end
+
 puts run(input, &:succ)
 puts run(input) { |n| n + (n >= 3 ? -1 : 1) }
 
