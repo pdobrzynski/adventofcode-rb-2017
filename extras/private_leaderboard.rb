dc870d80d3511c655568d7283cbc5c622fff722d
Peter Tseng <pht24@cornell.edu>
2017-12-19 00:59:33 -0800

    private_leaderboard: print score

--- extras/private_leaderboard.rb
+++ extras/private_leaderboard.rb
@@ -5,6 +5,7 @@ people = ARGV.flat_map { |a| JSON.parse(File.read(a))['members'].values }
 
 max_day = people.flat_map { |v| v['completion_day_level'].keys }.map(&:to_i).max
 longest_name = people.map { |v| v['name'] || v['id'] }.map(&:size).max
+score = Hash.new(0)
 
 (1..max_day).each { |day|
   (1..2).each { |part|
@@ -15,8 +16,14 @@ longest_name = people.map { |v| v['name'] || v['id'] }.map(&:size).max
     puts "Day #{day} Part #{part}:"
     fmt = "%#{longest_name}s %s %d"
     completing_people.sort_by(&:last).each_with_index { |(p, t), i|
+      score[p] += people.size - i
       puts fmt % [p, t, people.size - i]
     }
     puts
   }
+}
+
+fmt = "%#{longest_name}s %d"
+score.sort_by(&:last).reverse_each { |x|
+  puts fmt % x
 }
