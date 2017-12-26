2ddf38075a4c0c47f2efc650252ca6d014ee6645
Peter Tseng <pht24@cornell.edu>
2017-12-25 16:50:50 -0800

    Day 25: (fail) run 4 iterations at a time
    Makes it take about twice as long, since we read/write 7 elements for
    every 4 cycles instead of the previous 1:1 trade.
    
    We could consider counting the number of cycles for which we stay in a
    neighbourhood, but that comes later.

--- 25_turing_machine.rb
+++ 25_turing_machine.rb
@@ -18,7 +18,7 @@ end
 
 input = (ARGV.empty? ? DATA : ARGF).readlines
 
-state = parse_state(input.shift)
+START_STATE = parse_state(input.shift)
 check_after = Integer(input.shift[/\d+/])
 
 states = []
@@ -35,18 +35,40 @@ until input.empty?
   }
 end
 
+cache = {}
+BLOCK_SIZE = 7
+BLOCK_RADIUS = BLOCK_SIZE / 2
+
+(0...(2 ** BLOCK_SIZE)).each { |x|
+  bits = (0...BLOCK_SIZE).map { |bit_i| x & (1 << (BLOCK_SIZE - 1 - bit_i)) != 0 ? 1 : 0 }
+  states.each_index { |start_state|
+    state = start_state
+    ticker = bits.dup
+    pos = BLOCK_RADIUS
+    (BLOCK_RADIUS + 1).times {
+      ticker[pos], where_to_go, state = states[state][ticker[pos]]
+      pos += where_to_go
+    }
+    raise 'conflict' if cache.has_key?(start_state << BLOCK_SIZE | x)
+    cache[start_state << BLOCK_SIZE | x] = [state, ticker.freeze, pos - BLOCK_RADIUS].freeze
+  }
+}
+
 ticker = [0] * (check_after ** 0.5).ceil * 2
 pos = ticker.size / 2
+state = START_STATE
 
-check_after.times {
-  ticker[pos], where_to_go, state = states[state][
-    ticker[pos] || 0.tap { ticker.concat([0] * ticker.size) }
-  ]
-  pos += where_to_go
-  if pos < 0
+(check_after / (BLOCK_RADIUS + 1)).times {
+  x = (-BLOCK_RADIUS..BLOCK_RADIUS).reduce(0) { |acc, dp| acc << 1 | (
+    ticker[pos + dp] || 0.tap { ticker.concat([0] * ticker.size) }
+  )}
+  state, new_bits, delta_pos = cache[state << BLOCK_SIZE | x]
+  if pos - BLOCK_RADIUS < 0
     pos += ticker.size
     ticker.unshift(*[0] * ticker.size)
   end
+  ticker[pos - BLOCK_RADIUS, BLOCK_SIZE] = new_bits
+  pos += delta_pos
 }
 
 puts ticker.sum
